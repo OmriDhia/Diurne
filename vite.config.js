6045c9aa45034f79e24616437e6d9aa746d2b011
Youssef Abbassi <mustapha@beampulse.com>
2024-04-14 16:25:06 +0100

    bugfixes .htaccess

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
+import { copy } from 'vite-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,12 @@ export default defineConfig({
         vueI18n({
             include: path.resolve(__dirname, "./src/locales/**"),
         }),
+        copy({
+            targets: [
+              { src: '.htaccess', dest: 'dist' } // Adjust 'dist' to your build output directory
+            ],
+            hook: 'writeBundle' // Run the copy operation after the build is complete
+          })
     ],
     optimizeDeps: {
         include: ["quill", "nouislider"],
