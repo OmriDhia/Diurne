f297a8ca082492efac71d4d5ab14516ab6201281
YassineOPS-dev <bensalha.yassine1@gmail.com>
2025-02-10 15:59:59 -0800

    task 214 error handling+ fixed bugs on passing Float to Store State Management

--- src/components/projet/contremarques/_Partials/d-materials-list.vue
+++ src/components/projet/contremarques/_Partials/d-materials-list.vue
@@ -3,12 +3,10 @@
         <div class="row align-items-start" v-if="showTitle">
             <h6 class="w-100 p-0">Matière demandés</h6>
         </div>
-        <div class="card p-0">
-            <perfect-scrollbar tag="div" class="h-130-forced p-0"
-                               :options="{ wheelSpeed: 0.5, swipeEasing: !0, minScrollbarLength: 40, maxScrollbarLength: 130, suppressScrollX: true }">
-
+        <div class="card p-0" :class="{ 'border border-danger shadow-sm': error }">
+            <perfect-scrollbar tag="div" class="h-130-forced p-0" :options="{ wheelSpeed: 0.5, swipeEasing: !0, minScrollbarLength: 40, maxScrollbarLength: 130, suppressScrollX: true }">
                 <div class="card-body p-0 ps-2 mt-2">
-                    <template v-for="(material,index) in materials">
+                    <template v-for="(material, index) in materials">
                         <div class="row align-items-center justify-content-between ps-0">
                             <div class="col-6">
                                 <d-materials-dropdown :disabled="disabled" :hideLabel="true" v-model="material.material_id"></d-materials-dropdown>
@@ -25,26 +23,34 @@
                     </template>
                 </div>
             </perfect-scrollbar>
+            <!-- <div v-if="error" class="text-danger">
+                Le taux total des matières doit être au moins 100.
+            </div> -->
+            <!-- Error Message -->
         </div>
         <d-modal-add-material @addMaterial="addMaterial($event)"></d-modal-add-material>
-        <div class="col-12 ps-0 mt-2">
+        <div class="col-12 ps-0 mt-2 d-flex align-items-center">
             <div class="col-auto ps-0">
                 <button :disabled="disabled" class="btn ms-0 btn-outline-custom" data-bs-toggle="modal" data-bs-target="#modalAddMaterials">
                     Ajouter
                     <vue-feather type="plus" size="14"></vue-feather>
                 </button>
             </div>
+            <!-- Error Message (Appears Next to Button) -->
+            <transition name="fade">
+                <span v-if="error" class="ms-3 text-danger fw-bold"> Le taux total des matières doit être au moins 100. </span>
+            </transition>
         </div>
     </div>
 </template>
 
 <script>
     import axiosInstance from '../../../../config/http';
-    import dModalAddMaterial from "../_Partials/d-modal-add-material.vue";
-    import dDelete from "../../../common/d-delete.vue";
+    import dModalAddMaterial from '../_Partials/d-modal-add-material.vue';
+    import dDelete from '../../../common/d-delete.vue';
     import VueFeather from 'vue-feather';
-    import DPanelTitle from "../../../common/d-panel-title.vue";
-    import DMaterialsDropdown from "../dropdown/d-materials-dropdown.vue";
+    import DPanelTitle from '../../../common/d-panel-title.vue';
+    import DMaterialsDropdown from '../dropdown/d-materials-dropdown.vue';
 
     export default {
         components: {
@@ -52,40 +58,45 @@
             DPanelTitle,
             dModalAddMaterial,
             VueFeather,
-            dDelete
+            dDelete,
         },
         props: {
-            materialsProps:{
-                type: Array
+            materialsProps: {
+                type: Array,
             },
-            firstLoad : {
-                type: Boolean
+            firstLoad: {
+                type: Boolean,
             },
             disabled: {
                 type: Boolean,
-                default: false
+                default: false,
             },
             showTitle: {
                 type: Boolean,
-                default: true
-            },  
+                default: true,
+            },
+            error: {
+                type: Boolean,
+                default: false,
+            },
         },
         data() {
             return {
                 materials: [],
-                selectedLocation: null
+                selectedLocation: null,
             };
         },
         methods: {
             formatDataProps() {
-                this.materials = this.materialsProps.map(m => ({
+                this.materials = this.materialsProps.map((m) => ({
                     material_id: m.material_id,
-                    rate: parseFloat(m.rate)
+                    rate: parseFloat(m.rate),
                 }));
                 this.updateMaterialsInStore();
             },
-            addMaterial(data) {
-                this.materials.push(data);
+            addMaterial(newMaterial) {
+                newMaterial.rate = parseFloat(newMaterial.rate);
+                this.materials.push(newMaterial);
                 this.updateMaterialsInStore();
                 this.$emit('changeMaterials', this.materials);
             },
@@ -95,21 +106,30 @@
                 this.$emit('changeMaterials', this.materials);
             },
             updateMaterialsInStore() {
-                this.$store.commit("setMaterials", this.materials);
-            }
+                this.$store.commit('setMaterials', this.materials);
+            },
         },
         watch: {
             materialsProps() {
                 this.formatDataProps();
-                if(!this.firstLoad){
+                if (!this.firstLoad) {
                     this.$emit('changeMaterials', this.materials);
                 }
-            }
-        }
+            },
+        },
     };
 </script>
 <style scoped>
     ul {
         list-style-type: none;
+    }
+    /* Fade effect for error message */
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.3s ease-in-out;
+    }
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0;
     }
 </style>
