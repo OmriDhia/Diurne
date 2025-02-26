64a1864e0840061cfee42aee2b7f7a84cc700300
Anis Ajengui <48988628+ajenguianis@users.noreply.github.com>
2025-02-26 18:12:05 +0100

    Update d-modal-manage-locations.vue

--- src/components/projet/contremarques/_Partials/d-modal-manage-locations.vue
+++ src/components/projet/contremarques/_Partials/d-modal-manage-locations.vue
@@ -92,9 +92,7 @@
         try{
             data.value.price_min = parseFloat(data.value.price_min);
             data.value.price_max = parseFloat(data.value.price_max);
-            data.value.quote_processing_date = (typeof data.value.quote_processing_date === 'object' && Object.keys(data.value.quote_processing_date).length > 0)
-                ? Helper.FormatDate(data.value.quote_processing_date, "YYYY-MM-DD HH:mm:ss")
-                : "";
+            data.value.quote_processing_date = "";
             data.value.createdAt = data.value.createdAt ? Helper.FormatDate(data.value.createdAt,"YYYY-MM-DD HH:mm:ss") : Helper.FormatDate(new Date(),"YYYY-MM-DD HH:mm:ss");
             data.value.contremarqueId = props.contremarqueId;
             if(data.value.location_id){
@@ -130,7 +128,7 @@
                 carpetTypeId: loc.carpetType_id,
                 description: loc.description,
                 quote_processed: loc.quote_processed,
-                quote_processing_date: (loc.quote_processing_date) ? Helper.FormatDate(loc.quote_processing_date.date,'YYYY-MM-DD HH:mm:ss') : "",
+                quote_processing_date: "",
                 price_min: Helper.FormatNumber(loc.price_min),
                 price_max: Helper.FormatNumber(loc.price_max),
                 createdAt: loc.created_at.date ? new Date(loc.created_at.date) : new Date(),
