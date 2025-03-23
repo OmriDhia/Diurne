c93dbfedef46edb58b7a6e886d4ff841b306c63f
ahmedguesmi <ahmedguesmi.sb@gmail.com>
2025-03-23 07:53:44 +0100

    Créer la page modèle carrier currency

--- src/components/settings/d-devise.vue
+++ src/components/settings/d-devise.vue
@@ -1,6 +1,6 @@
 <template>
   <d-data-grid :fetchData="fetchData" :saveData="saveData" :addData="addData" :deleteData="deleteData"
-    :columns="columns" :rows="rows" title="Model" rowKey="id" />
+    :columns="columns" :rows="rows" title="Devise" rowKey="id" />
 </template>
 
 <script setup>
@@ -9,21 +9,26 @@
   import axiosInstance from '../../config/http';
 
   const columns = [
-    { key: "code", label: "Code", type: "text" },
-    { key: "number_max", label: "Number Max", type: "number" },
+    { key: "name", label: "Nom", type: "text" }
   ];
 
-  const rows = ref([]);
 
-  const fetchData = async ({ page, itemsPerPage }) => {
+  const rows = ref([]);
+  const totalItems = ref(null);
+  const totalPages = ref(null);
+
+  const fetchData = async ({ page, itemsPerPage } = {}) => {
     try {
-      const { data } = await axiosInstance.get('/api/models', {
-        params: {
-          page,
-          itemsPerPage
-        }
-      });
+      const params = {};
+      if (page !== undefined && itemsPerPage !== undefined) {
+        params.page = page;
+        params.itemsPerPage = itemsPerPage;
+      }
+
+      const { data } = await axiosInstance.get('/api/currency', { params });
+
       return data;
+      
     } catch (error) {
       console.error('Erreur lors de la récupération des données:', error);
       throw new Error('Failed to fetch data');
@@ -32,16 +37,17 @@
 
   const addData = async (row) => {
     try {
-      const { data } = await axiosInstance.post('/api/models', row);
-      rows.value.push(data.response.data);
-      return data.response.data;
+      const { data } = await axiosInstance.post('/api/createCurrency', row);
+      rows.value.push(data.response);
+      return data.response;
     } catch (error) {
       if (error.response && error.response.status === 409) {
-          const errorMessage = "Un modèle avec ce code existe déjà.";
-          window.showMessage(errorMessage, "error");
-      }else{
-          window.showMessage("Une erreur inattendue s'est produite lors de l'ajout.", "error");
+        const errorMessage = "Un Transporteur avec ce code existe déjà.";
+        window.showMessage(errorMessage, "error");
+      } else {
+        window.showMessage("Une erreur inattendue s'est produite lors de l'ajout.", "error");
       }
+
       console.error('Erreur lors de l\'ajout des données:', error);
       throw new Error('Failed to add data');
     }
@@ -50,8 +56,8 @@
   const deleteData = async (row) => {
     console.log(row);
     try {
-      await axiosInstance.delete(`/api/models/${row.id}`);
-      rows.value = rows.value.filter(item => item.id !== row.id); 
+      await axiosInstance.delete(`/api/currency/${row.id}`);
+      rows.value = rows.value.filter(item => item.id !== row.id);
     } catch (error) {
       console.error('Erreur lors de la suppression des données:', error);
       throw new Error('Failed to delete data');
@@ -60,12 +66,12 @@
 
   const saveData = async (row) => {
     try {
-      const { data } = await axiosInstance.put(`/api/models/${row.id}`, row);
+      const { data } = await axiosInstance.put(`/api/updateCurrency/${row.id}`, row);
       const index = rows.value.findIndex(item => item.id === row.id);
       if (index !== -1) {
-        rows.value[index] = data.response.data; 
+        rows.value[index] = data.response;
       }
-      return data.response.data;
+      return data.response;
     } catch (error) {
       console.error('Erreur lors de la mise à jour des données:', error);
       throw new Error('Failed to save data');
