5cf521f7c90a1b36341369732a5c2f757110f15e
Yousouf <taki-eddine-echeikh.omar@tek-up.tn>
2025-06-22 18:38:16 +0100

    Add workshop information creation

---
+++ src/Services/workshop-service.js
@@ -0,0 +1,21 @@
+import axiosInstance from '../config/http';
+
+export default {
+  async createWorkshopInformation(data) {
+    const res = await axiosInstance.post('/api/workshopInformation', data);
+    return res.data;
+  },
+  async generateRN(manufacturerId, imageCommandId) {
+    const res = await axiosInstance.post('/api/carpet', {
+      manufacturerId,
+      imageCommandId,
+    });
+    return res.data;
+  },
+  async getManufacturers({ page = 0, itemsPerPage = 10 } = {}) {
+    const res = await axiosInstance.get('/api/manufacturer', {
+      params: { page, itemsPerPage },
+    });
+    return res.data;
+  },
+};
