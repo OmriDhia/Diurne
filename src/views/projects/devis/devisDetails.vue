639a135da0a5dc4db82b1dc343f5cfc85cb08e25
OmriDhia <dhiaomri94@gmail.com>
2025-08-15 16:50:17 +0100

    Uncheck proposed discount when calculating from total HT

--- src/views/projects/devis/devisDetails.vue
+++ src/views/projects/devis/devisDetails.vue
@@ -849,6 +849,15 @@
         { deep: true }
     );
 
+    watch(
+        () => data.value.quoteDetail.calculateFromTotalExcludingTax,
+        (val) => {
+            if (val) {
+                data.value.quoteDetail.applyProposedDiscount = false;
+            }
+        }
+    );
+
     const confirmHandle = async () => {
         new window.Swal({
             title: 'Êtes-vous sûr ?',
